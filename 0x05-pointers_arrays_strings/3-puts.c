c96f8be9ca38163e667decab7bdb83b35d845b58
Marinus12 <dubemarinus@gmail.com>
2023-03-27 07:27:26 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _puts - Prints a string
+  * @str: a param to _puts function
+  */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
