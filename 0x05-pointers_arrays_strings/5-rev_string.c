63fa8ce99d167683fbb3ddbb84ec5214ae4acaf3
Marinus12 <dubemarinus@gmail.com>
2023-03-27 07:35:25 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be modified
+ *
+ * Return: void
+*/
+
+void rev_string(char *s)
+{
+	int l, i;
+	char ch;
+
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+
+	for (i = 0; i < l / 2; ++i)
+	{
+		ch = s[i];
+		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
+		s[l - 1 - i] = ch;
+	}
+
+}
