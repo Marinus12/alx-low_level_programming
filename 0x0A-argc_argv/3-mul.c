8b96805d7a5b0c5e0f61463c99a67783774d4fe9
Ukewuihe Marinus <dubemarinus@gmail.com>
2023-10-03 09:21:00 -0700

    print the result of the multiplication, followed by a new line

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * main - prints Multiplies two numbers
+  * @argc:  Counts arguments
+  * @argv: Arguments vector
+  * Return: 0 if works,1 if there's ERROR
+  */
+int main(int argc char *argv[])
+{
+	int n, m, diff;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+	diff - n % m;
+
+	printf("%i\n", diff);
+
+	return (0);
+}
