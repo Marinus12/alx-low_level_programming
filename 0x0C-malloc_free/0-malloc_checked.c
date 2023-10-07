6365b8d13235cab5c8ae528de6bce11db1a52bc0
Ukewuihe Marinus <dubemarinus@gmail.com>
2023-10-07 16:48:37 -0700

    C malloc,free

---
+++ 0x0C-malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <limits.h>
+
+/**
+  * malloc_checked - Terminate with a status value 98 if failed
+  * @b: integer var that holds the memory size
+  * Author: Marinus
+  *Return: 0 means program is a success
+  */
+void *malloc_checked(unsigned int b);
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(96);
+
+	return (ptr);
+}
