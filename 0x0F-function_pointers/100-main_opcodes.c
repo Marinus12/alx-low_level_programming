943173a67c5763c164d8a1866487789696030e6d
Ukewuihe Marinus <dubemarinus@gmail.com>
2023-10-12 09:28:06 -0700

    C - Function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+
+/**
+  * main - writes opcodes of its main function
+  * @argc: number of arguments
+  * @argv: array of arguments
+  *
+  * Return: Always 0 (Success)
+  */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes -1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%d02hhx ", arr[i]);
+	}
+
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - searches for an integer
+  * @size: number of elements in an array
+  * @cmp: pointer to the function
+  * @array: an array of integers to search
+  * Return: 0 if false
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
