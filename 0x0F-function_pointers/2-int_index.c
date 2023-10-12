344b1d2645405d9e5ebcd076568a6c21ec641709
Ukewuihe Marinus <dubemarinus@gmail.com>
2023-10-12 09:45:00 -0700

    C - Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,19 +9,22 @@
   */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		return (-1);
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
+		if (array != NULL && cmp != NULL)
 		{
-			return (i);
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
 		}
 	}
+
+
 	return (-1);
 }
