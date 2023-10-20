df5e52e9540375ec5200d08682a724e9176a42ad
Ukewuihe Marinus <dubemarinus@gmail.com>
2023-10-20 07:09:27 -0700

    C Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * first_line - prints nefore the main function
+  *
+  * Return: line before main
+  */
+void first_line(void) __attribute__((constructor));
+void first_line(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house on my back!\n");
+}
