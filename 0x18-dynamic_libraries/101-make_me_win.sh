30e546a3d06dacb569bad98bc2c65c0759a3de9b
Ukewuihe Marinus <dubemarinus@gmail.com>
2023-12-11 05:16:06 -0800

    Hack for Jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P
+wget -P .. https://raw.githubusercontent.com/Marinus12/alx-low_level_programming/master/0x18-dynamics_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
