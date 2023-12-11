f966e56e178666ea7bfb26f5fb36ad74251ea8b6
Ukewuihe Marinus <dubemarinus@gmail.com>
2023-12-11 14:57:42 -0800

    Dynamic Library liball.so

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -c *.c -fpic && gcc *.o -shared -o liball.so
+gcc -c 0-isupper.c 1-memcpy.c 0-main.c 0-memset.c 0-strcat.c 100-atoi.c 1-isdigit.c 1-memcpy.c 3-islower.c 3-puts.c 3-strcmp.c 3-strspn.c  -fpic && gcc *.o -shared -o liball.so
